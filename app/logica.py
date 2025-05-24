5e43473aa04c005e008d154f7c5c0b791c6d5929
charles665 <jaguilarestacio@gmail.com>
2025-05-24 11:48:52 -0500

    Trabajo con correcion en los nombres de los achivos

--- app/logica.py
+++ app/logica.py
@@ -27,21 +27,21 @@ class Participante:
             "id": self.id,
             "nombre": self.nombre,
             "edad": self.edad,
-            "taller_inscrito": self.taller_inscrito,
-            "mes_participacion": self.mes_participacion,
-            "clases_asistidas": self.clases_asistidas,
-            "valor_clase": clase_precios.get(self.taller_inscrito, 0),
-            "total_pagado": self.calcular_pago()
+            "taller": self.taller_inscrito,
+            "mes": self.mes_participacion,
+            "clases asistidas": self.clases_asistidas,
+            "valor clase": clase_precios.get(self.taller_inscrito, 0),
+            "total pagado": self.calcular_pago()
         }
 
-
 class GestionParticipantes:
-   
-    def __init__(self, archivo='app/participantes.csv'):
+    campos = ["id", "nombre", "edad", "taller", "mes", "clases asistidas", "valor clase", "total pagado"]
+
+    def __init__(self, archivo='participantes.csv'):
         self.archivo = archivo
         self.contador_id = self.obtener_ultimo_id() + 1
-        self.campos = ["id", "nombre", "edad", "taller_inscrito", "mes_participacion","clases_asistidas", "valor_clase", "total_pagado"
-]
+
+   
 
     def mostrar_participantes(self):
         if os.path.isfile(self.archivo):
@@ -51,8 +51,6 @@ class GestionParticipantes:
 
     def añadir_participante(self, nombre, edad, taller_inscrito, mes_participacion, clases_asistidas):
         nuevo = Participante(self.contador_id, nombre, edad, taller_inscrito, mes_participacion, clases_asistidas)
-        participante=nuevo.reporte_participante()
-        print(participante)
         with open(self.archivo, 'a', newline='', encoding='utf-8') as f:
             writer = csv.DictWriter(f, fieldnames=self.campos)
             if os.path.getsize(self.archivo) == 0:
@@ -61,6 +59,7 @@ class GestionParticipantes:
         self.contador_id += 1
     
     def modificar_participante(self, id, nombre, edad, taller_inscrito, mes_participacion, clases_asistidas):
+
         df=self.mostrar_participantes()
         df['id'] = df['id'].astype(str)
         id = str(id)
@@ -72,10 +71,12 @@ class GestionParticipantes:
             df.loc[df['id'] == id, 'clases_asistidas'] = clases_asistidas
 
             df.to_csv(self.archivo, index=False)
-            return True  
-        
+            return True     
         else:
+            print("ID no encontrado.")
             return False
+            
+        
     
     def eliminar_receta_por_id(self, id_eliminar):
         df=self.mostrar_participantes()
@@ -89,12 +90,23 @@ class GestionParticipantes:
         else:
             return False
 
-    # def actualizar_archivo(self):
-    #     return self.mostrar_participantes()
+
+    def actualizar_archivo(self):
+        return self.mostrar_participantes()
     
+     
     def obtener_ultimo_id(self):
         if os.path.isfile(self.archivo):
-             df = self.mostrar_participantes()
+             df = pd.read_csv(self.archivo)
              if not df.empty:
                  return df["id"].max()
         return 0
+    def buscar_participante(self, busqueda):
+      df = pd.read_csv("participantes.csv")
+      df["id"] = df["id"].astype(str)
+      busqueda = str(busqueda)
+      if busqueda in df["id"].values:
+          busqueda_fila = df[df["id"] == busqueda]
+          return busqueda_fila.to_string(index=False)
+      else:
+          return 'No esta registrado ningun usuario con ese id'
